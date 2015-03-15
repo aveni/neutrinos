12bdcedfc9b7e97a35099e2806b79bc99a39c755
Abhinav Venigalla <abhinav.venigalla@gmail.com>
2015-03-15 01:01:43 -0400

    changed team name to string

--- db/migrate/20150314015328_create_teams.rb
+++ db/migrate/20150314015328_create_teams.rb
@@ -3,7 +3,7 @@ class CreateTeams < ActiveRecord::Migration
     create_table :teams do |t|
 
     	t.integer :number
-    	t.text :name
+    	t.string :name
       t.timestamps null: false
     end
   end
