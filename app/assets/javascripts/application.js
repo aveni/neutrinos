63970d64b8507acfe5b6a19d6e7b99010a2d5143
Abhinav Venigalla <abhinav.venigalla@gmail.com>
2015-03-22 20:12:39 -0400

    database change, faster

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require_tree .
