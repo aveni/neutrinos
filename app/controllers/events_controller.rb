f7fdef453679e1309790e25264fb6764c043ee43
Abhinav Venigalla <abhinav.venigalla@gmail.com>
2015-03-29 13:42:20 -0400

    small mistake

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 class EventsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 	load_and_authorize_resource
+	include TeamsHelper
+	include MatchesHelper
 
 	def index
 			@events = Event.all.order(:name)
