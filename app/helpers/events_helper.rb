d7625281e250d352a70399a944db48cde2b407e7
Abhinav Venigalla <abhi@galois.andover.edu>
2015-04-07 20:54:08 -0400

    QP added

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -9,6 +9,49 @@ module EventsHelper
 		content_tag(:span, event.level, class: "label label-warning label-as-badge")
 	end
 
+	def QP(event)
+		teams = event.teams
+
+		map = {}
+		for i in 0..teams.size-1
+			map[teams[i].number] = i
+		end
+
+		arr = Array.new(teams.size) {Array.new(2) {0}}		
+
+		event.matches.includes(:red1, :red2, :blue1, :blue2).each do |m|
+			r1 = map[m.red1.number]
+			r2 = map[m.red2.number]
+			b1 = map[m.blue1.number]
+			b2 = map[m.blue2.number]
+
+			arr[r1][0] += 1
+			arr[r2][0] += 1
+			arr[b1][0] += 1
+			arr[b2][0] += 1
+
+			n = winningAlliance(m)
+			if n == -1
+				arr[r1][1] += 2
+				arr[r2][1] += 2
+			elsif n == 1
+				arr[b1][1] += 2
+				arr[b2][1] += 2
+			else
+				arr[r1][1] += 1
+				arr[r2][1] += 1
+				arr[b1][1] += 1
+				arr[b2][1] += 1	
+			end
+		end
+
+		hash = {}
+		map.each do |k,v|
+			hash[k] = arr[v]
+		end
+		hash
+	end
+
 
 	def OPR(event)
 		teams = event.teams
